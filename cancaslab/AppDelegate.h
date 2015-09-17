7de9de9fd9ca852e375a21a325adcd674a498abb
Amit Agarwal <lifeamit@gmail.com>
2015-09-17 12:38:21 -0700

    initial commit

---
+++ cancaslab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cancaslab
+//
+//  Created by Amit Agarwal on 9/17/15.
+//  Copyright © 2015 Amit Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cancaslab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cancaslab
+//
+//  Created by Amit Agarwal on 9/17/15.
+//  Copyright © 2015 Amit Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
